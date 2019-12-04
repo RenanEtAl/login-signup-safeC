0befce063f16817d415c7221bbd0252085c5641e
Renanie Tanola <enan.tanola@gmail.com>
2019-12-04 08:53:18 -0800

    config

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://ren:'+ encodeURIComponent('cLkcMkK1lWRw6szF') + '@cluster0-eaovm.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
